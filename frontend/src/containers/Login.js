4c701139b4d32f0764ef02e53e9293ccabfd70c4
andybrvt <andybrvt@gmail.com>
2020-03-04 14:43:08 -0700

    loading

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,20 +1,30 @@
 import React from 'react';
-
+import './Login.css'
 import { Form, Icon, Input, Button, Spin } from 'antd';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 const FormItem = Form.Item;
 
-const antIcon = <Icon type="loading" style=
-  "
-    text-align: center;
-    background: rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-    margin-bottom: 20px;
-    padding: 30px 50px;
-    margin: 20px 0;
-  "
-   spin />
+
+
+// const antIcon = <Icon type="loading" style=
+//   "
+//     text-align: center;
+//     background: rgba(0, 0, 0, 0.05);
+//     border-radius: 4px;
+//     margin-bottom: 20px;
+//     padding: 30px 50px;
+//     margin: 20px 0;
+//   "
+//    spin />
+
+// {
+//
+// this.props.loading ?
+//
+//   <Spin indicator={antIcon} />
+//
+//   :
 
 class Login extends React.Component {
   handleSubmit = (e) => {
@@ -31,47 +41,45 @@ class Login extends React.Component {
     const { getFieldDecorator } = this.props.form;
       return (
 
+        <div className ="example">
+
+          {
+            this.props.loading?
+            <Spin />
+
+            :
+
+            <Form onSubmit =  {this.handleSubmit} className = "login-form">
 
 
-        <div>
-        {
+            <FormItem>
+            {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
+            </FormItem>
 
-        this.props.loading ?
+            <FormItem>
+            {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            )}
+            </FormItem>
 
-                <Spin indicator={antIcon} />
+            <FormItem>
+              <Button type = "primary" htmltype="submit" style={{marginRight: '10px'}}>
+                Login
+              </Button>
+                Or
+              <NavLink style={{marginRight: '10px' }} to ='/signup/'>
+                Signup
+              </NavLink>
+            </FormItem>
 
-                :
-          
-          <Form onSubmit =  {this.handleSubmit} className = "login-form">
-
-
-          <FormItem>
-          {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
-          </FormItem>
-
-          <FormItem>
-          {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
-          </FormItem>
-
-          <FormItem>
-            <Button type = "primary" htmltype="submit" style={{marginRight: '10px'}}>
-              Login
-            </Button>
-              Or
-            <NavLink style={{marginRight: '10px' }} to ='/signup/'>
-              Signup
-            </NavLink>
-          </FormItem>
-
-          </Form>
+            </Form>
+        }
         </div>
 
       );
