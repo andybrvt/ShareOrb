215a47b09b67eb521ee3d614a56e1510e83d1059
andybrvt <andybrvt@gmail.com>
2020-03-10 16:39:45 -0700

    signup fixup

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -11,20 +11,64 @@ class Signup extends React.Component {
     state = {
       confirmDirty: false,
     };
+    //this handle submit is a funciton that handles
+    compareToFirstPassword = (rule, value, callback) => {
+      debugger;
+      const form = this.props.form;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords do not match')
+      } else {
+        callback();
+      }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+      debugger;
+      const form = this.props.form;
+      if (value && this.state.confirmDirty) {
+        form.validateFields(['confirm'], {force: true});
+      }
+      callback();
+    }
+
+    validateLength = (rule, value, callback) => {
+      if (value.length < 8 && value.length > 1) {
+        callback('Password must be at least 8 characters!')
+      } else {
+      callback();
+      }
+    }
+
+    validateUpper = (rule, value, callback) => {
+      if(value.search(/[A-Z]/) < 0) {
+        return callback('Password must have a upper case letter!')
+      }else {
+        callback();
+        }
+      }
+
+    validateNumeric = (rule, value, callback) => {
+      if(value.search(/[0-9]/) < 0) {
+        return callback('Password must have a number!')
+      }else {
+        callback();
+        }
+      }
 
     handleSubmit = (e) => {
       e.preventDefault();
       this.props.form.validateFieldsAndScroll((err, values) => {
-        if (!err) {
+        console.log(values.password.length)
+        console.log(err)
+        if (!err && values.password.length > 8) {
           this.props.onAuth(
             values.userName,
-            values.password,
+            values.email,
             values.password,
             values.confirm,
           );
-
+          this.props.history.push('/');
         }
-        this.props.history.push('/');
     });
   }
 
@@ -32,6 +76,9 @@ class Signup extends React.Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
+    console.log('test')
+    console.log(this.props)
+    console.log(getFieldDecorator)
       return (
         <Form onSubmit={this.handleSubmit}>
         <FormItem>
@@ -60,9 +107,15 @@ class Signup extends React.Component {
               required: true, message: 'Please input your password!',
             }, {
               validator: this.validateToNextPassword,
+            }, {
+              validator: this.validateLength,
+            }, {
+              validator: this.validateUpper,
+            }, {
+              validator: this.validateNumeric,
             }],
           })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"/>
           )}
         </FormItem>
 
