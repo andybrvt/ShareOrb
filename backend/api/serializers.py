3341574cbc3bb1518c63fdde14b58df5790dd931
ual-laptop <pinghsu520@gmail.com>
2020-03-12 02:31:01 -0700

    fix your dog shit change

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,123 +4,13 @@ from . import models
 
 # this converts model data into json and you will connection
 # this to the views
-# class ProfileSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		fields = (
-#
-# 			'id',
-# 			'first_name',
-#     		'last_name',
-#     		 # 'email',
-# 		)
-# 		model = models.Profile
-
-class AccountEmailaddressSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AccountEmailaddress
-		fields = "__all__"
-
-class AccountEmailconfirmationSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AccountEmailconfirmation
-		fields = "__all__"
-
 class NewsFeedSerializers(serializers.ModelSerializer):
 	class Meta:
-		model= models.Newsfeed
-		fields = "__all__"
+		fields = (
 
-class AuthGroupSerialziers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthGroup
-		fields = "__all__"
-
-class AuthGroupPermissionsSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthGroupPermissions
-		fields = "__all__"
-
-class AuthPermissionSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthPermission
-		fields = "__all__"
-
-class AuthUserSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthUser
-		fields = "__all__"
-
-class AuthUserGroupsSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthUserGroups
-		fields = "__all__"
-
-class AuthtokenTokenSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AuthtokenToken
-		fields = "__all__"
-
-class DjangoAdminLogSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.DjangoAdminLog
-		fields = "__all__"
-
-class DjangoContentTypeSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.DjangoContentType
-		fields = "__all__"
-
-class DjangoMigrationsSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.DjangoMigrations
-		fields = "__all__"
-
-class DjangoSessionSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.DjangoSession
-		fields = "__all__"
-
-class DjangoSiteSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.DjangoSite
-		fields = "__all__"
-
-class SocialaccountSocialaccountSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.SocialaccountSocialaccount
-		fields = "__all__"
-
-class SocialaccountSocialappSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.SocialaccountSocialapp
-		fields = "__all__"
-
-class SocialaccountSocialappSites(serializers.ModelSerializer):
-	class Meta:
-		model= models.SocialaccountSocialappSites
-		fields = "__all__"
-
-class SocialaccountSocialtokenSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.SocialaccountSocialtoken
-		fields = "__all__"
-
-class StudentSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.Student
-		fields = "__all__"
-
-class AccountEmailaddressSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AccountEmailaddress
-		fields = "__all__"
-
-class AccountEmailaddressSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AccountEmailaddress
-		fields = "__all__"
-
-class AccountEmailaddressSerializers(serializers.ModelSerializer):
-	class Meta:
-		model= models.AccountEmailaddress
-		fields = "__all__"
+			'id',
+			'first_name',
+    		'last_name',
+    		 # 'email',
+		)
+		model = models.Newsfeed
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,13 +5,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from . import models
 from . import serializers
-# from .serializers import ProfileSerializer
 
-# view set that takes in a serializer by the serializer class
-# takes in a queryset in order to modify the objects
-# class ProfileViewSet(viewsets.ModelViewSet):
-# 	serializer_class = ProfileSerializer
-# 	queryset = models.Profile.objects.all()
 
 class NewsFeedViewSet(viewsets.ModelViewSet):
 	serializer_class = serializers.NewsFeedSerializers
