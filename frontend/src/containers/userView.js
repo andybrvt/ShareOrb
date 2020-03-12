08daa9f0641298cdf21bfe0f14d2962f0b94cf9c
ual-laptop <pinghsu520@gmail.com>
2020-03-11 17:30:26 -0700

    change

--- frontend/src/containers/userView.js
+++ frontend/src/containers/userView.js
@@ -4,7 +4,30 @@ import ProfilePost from '../components/Form';
 import {connect} from 'react-redux';
 class UserView extends React.Component {
 
+  state={
+		profileList:{
+      first_name:'ping',
+      last_name:'hsu',
+
+
+
+    },
+	}
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/newsfeed/1')
+      .then(res=> {
+        this.setState({
+          profileList:res.data,
+       });
+      });
+
+  }
+
+
 	render() {
+    console.log("THIS IS THE USER VIEW");
+    console.log(this.state.profileList);
 		return (
 			  <div>
           Initial test
