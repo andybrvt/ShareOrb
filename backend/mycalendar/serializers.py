e650e897b34d1cffebd3e0d79fb6cd71be205909
andybrvt <andybrvt@gmail.com>
2020-04-09 16:55:30 -0700

    What i have so far for calendars

--- backend/mycalendar/serializers.py
+++ backend/mycalendar/serializers.py
@@ -1,6 +1,34 @@
 from datetime import datetime
 from rest_framework import serializers
+import pytz
 
 
 from mycalendar import models
+from core.utils import DEFAULT_TIMEZONE, normalize_to_utc
+
 # from core.utils import
+
+class CalendarSerializer(serializers.HyperlinkModelSerializer):
+    """
+    Calendar serializers for admins, allows settings of owners
+    """
+    class Meta:
+        model = models.Calendar
+        fields = ('person', 'title')
+
+
+class CalendarOwnedSerializer(serializers.HyperlinkModelSerializer):
+    # Standard Calendar serializers for users
+    id = serializers.ReadyOnlyField()
+
+    class Meta:
+        model = models.Calendar
+        fields = ('person', 'title')
+
+class EventSerializer (serializers.HyperlinkModelSerializer):
+    # Event serializer for admins
+    id = serializer.ReadyOnlyField()
+
+    class Meta:
+        model = models.Event
+        friends = ('id', 'person', 'calendar', 'title', 'content', 'start_time', 'end_time', 'location' )
--- backend/mycalendar/views.py
+++ backend/mycalendar/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
+
 
 # Create your views here.
+def get_calendar(request):
+    all_events = Events.objects.all()
