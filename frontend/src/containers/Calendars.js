2fcf4028ac916500a74d3b95a375f5f80a67923a
andybrvt <andybrvt@gmail.com>
2020-04-12 00:26:30 -0700

    render our information on every day

--- frontend/src/containers/Calendars.js
+++ frontend/src/containers/Calendars.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { authAxios } from '../components/util';
 import { Calendar, Badge } from 'antd';
 import './Container_CSS/Calendar.css';
+import * as moment from 'moment';
 
 
 
@@ -30,64 +31,90 @@ class MyCalendar extends React.Component{
 
     })}
 
-  // componentWillReceiveProps(newProps){
-  //   axios.get('http://127.0.0.1:8000/userprofile/list/')
-  //   .this(res => {
-  //     this.setState({
-  //       evnets: res.data
-  //     })
-  //   })
-  // }
+  componentWillReceiveProps(newProps){
+    axios.get('http://127.0.0.1:8000/userprofile/list/')
+    .then(res => {
+      this.setState({
+        evnets: res.data
+      })
+    })
+  }
 
     getListData(value) {
-      console.log(value)
+
       let listData;
-      switch (value) {
-        case 8:
-          listData = [
-            { type: 'warning', content: 'This is warning event.' },
-            { type: 'success', content: 'This is usual event.' },
-          ];
-          break;
-        case 10:
-          listData = [
-            { type: 'warning', content: 'This is warning event.' },
-            { type: 'success', content: 'This is usual event.' },
-            { type: 'error', content: 'This is error event.' },
-          ];
-          break;
-        case 15:
-          listData = [
-            { type: 'warning', content: 'This is warning event' },
-            { type: 'success', content: 'This is very long usual event。。....' },
-            { type: 'error', content: 'This is error event 1.' },
-            { type: 'error', content: 'This is error event 2.' },
-            { type: 'error', content: 'This is error event 3.' },
-            { type: 'error', content: 'This is error event 4.' },
-          ];
-          break;
-        default:
+      console.log(value)
+      for (let i = 0; i < value.length; i++){
+        const event = value[i]
+        const stuff = event.start_time
+        const date_stuff = moment(stuff)
+        // console.log(event.content)
+        // console.log(date_stuff.toArray())
+        // console.log(stuff)
+        switch(date_stuff.date()){
+            case 9:
+              listData = [
+                {tile: event.title, content: event.content}
+              ]
+            default:
+        }
       }
+      // switch (value) {
+      //   case 9:
+      //     listData = [
+      //       { type: 'warning', content: 'This is warning event.' },
+      //     ];
+      //     break;
+      //   case 10:
+      //     listData = [
+      //       { type: 'warning', content: 'This is warning event.' },
+      //       { type: 'success', content: 'This is usual event.' },
+      //       { type: 'error', content: 'This is error event.' },
+      //     ];
+      //     break;
+      //   case 16:
+      //     listData = [
+      //       { type: 'warning', content: 'This is warning event' },
+      //       { type: 'success', content: 'This is very long usual event。。....' },
+      //       { type: 'error', content: 'This is error event 1.' },
+      //       { type: 'error', content: 'This is error event 2.' },
+      //       { type: 'error', content: 'This is error event 3.' },
+      //       { type: 'error', content: 'This is error event 4.' },
+      //     ];
+      //     break;
+      //   default:
+      // }
       return listData || [];
     }
 
 
-    dateCellRender(value) {
-  console.log(value)
-       const count = Number(value)
+    dateCellRender =(value,events) => {
 
-       const listData = [
-         { type: 'warning', content: 'This is warning event' },
-         { type: 'success', content: 'This is very long usual event。。....' },
-         { type: 'error', content: 'This is error event 1.' },
 
-       ];
-        // const listData = this.getListData(count);
+       // const count = Number(value)
+       // console.log(count)
+       // const eventList = {state.events}
+       // const listData = [
+       //   { type: 'warning', content: 'This is warning event' },
+       //   { type: 'success', content: 'This is very long usual event。。....' },
+       //   { type: 'error', content: 'This is error event 1.' },
+       //
+       // ];
+      // console.log(value)
+      for(let i = 0; i < value.length; i++){
+        // console.log(value[i])
+        const stuff = value[i].start_time
+        // console.log(stuff)
+      }
+
+      const listData = this.getListData(value);
+      console.log(listData)
         return (
           <ul className="events">
             {listData.map(item => (
-              <li key={item.content}>
-                <Badge status={item.type} text={item.content} />
+              <li key={item.title}>
+                {item.title}
+                {item.content}
               </li>
             ))}
           </ul>
@@ -104,9 +131,8 @@ class MyCalendar extends React.Component{
 
     monthCellRender(value) {
       console.log(value)
-        const count = Number(value)
-
-        const num = 5;
+        // const num = this.getMonthData(value)
+        const num = 5
           return num ? (
             <div className="notes-month">
               <section>{num}</section>
@@ -116,16 +142,17 @@ class MyCalendar extends React.Component{
         }
 
   render() {
-    console.log(this.state)
+    console.log(this.state.events)
     const time = this.state.events
     console.log(time)
     console.log(this.props);
     console.log(this.props.monthCellRender)
     // <Calendar />
-
+    // the datecellRender will run through each cell that is present in the current month, if the getListData is empty then it will not put anything
+    // if there is something tho then it will put something on
     return (
 
-      <Calendar dateCellRender={this.dateCellRender} monthCellRender={this.monthCellRender}/>
+      <Calendar dateCellRender={() => this.dateCellRender(this.state.events)} monthCellRender={this.monthCellRender}/>
 
     )
   };
