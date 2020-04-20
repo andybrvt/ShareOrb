c4570ecd904a2e8f7a1c57caf35956d362d6a541
pinghsu520 <pinghsu520@gmail.com>
2020-04-19 21:42:30 -0700

    glitch in dropdown right now

--- frontend/src/containers/GlobalHeader/ProfileDropDown.js
+++ frontend/src/containers/GlobalHeader/ProfileDropDown.js
@@ -58,7 +58,26 @@ import { authAxios } from '../../components/util';
   renderProfile() {
     if (this.props.isAuthenticated){
       return (
-      <div> Hi </div>
+        <Menu className={styles.menu} selectedKeys={[]} onClick={this.onMenuClick}>
+        { (
+          <Menu.Item key="center">
+            <UserOutlined />
+            个人中心
+          </Menu.Item>
+        )}
+        { (
+          <Menu.Item key="settings">
+            <SettingOutlined />
+            个人设置
+          </Menu.Item>
+        )}
+        { <Menu.Divider />}
+
+        <Menu.Item key="logout">
+          <LogoutOutlined />
+          退出登录
+        </Menu.Item>
+      </Menu>
     )
 
     } else {
@@ -81,31 +100,9 @@ import { authAxios } from '../../components/util';
   render() {
 
     const {
-
       menu,
     } = this.props;
-    const menuHeaderDropdown = (
-      <Menu className={styles.menu} selectedKeys={[]} onClick={this.onMenuClick}>
-        {menu && (
-          <Menu.Item key="center">
-            <UserOutlined />
-            个人中心
-          </Menu.Item>
-        )}
-        {menu && (
-          <Menu.Item key="settings">
-            <SettingOutlined />
-            个人设置
-          </Menu.Item>
-        )}
-        {menu && <Menu.Divider />}
 
-        <Menu.Item key="logout">
-          <LogoutOutlined />
-          退出登录
-        </Menu.Item>
-      </Menu>
-    );
 
     const test = true;
 
