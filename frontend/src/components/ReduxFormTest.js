c24f660252f34a0b9cb921bdf63a916113fa5ce1
andybrvt <andybrvt@gmail.com>
2020-05-05 23:19:36 -0700

    got the redux form to work

--- frontend/src/components/ReduxFormTest.js
+++ frontend/src/components/ReduxFormTest.js
@@ -1,39 +1,68 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import * as navActions from '../store/actions/nav';
+import * as calendarEventActions from '../store/actions/calendarEvent';
+import { connect } from "react-redux";
+
 
 // input component must be place in a Field component inorder to
 // have props such as value, onChange and stuff be passed in
 // Field basically replaces <input>
-class ReduxFormTest extends React.Component{
 
-    onSubmit = () => {
-      console.log('hi')
-    }
+const onSubmit = value => {
+  console.log(value)
+}
+
+// So in order to have more control over your input types you can make
+// your own components and pass them into the compoents of field
+// The parameter can be pretty much anything
+// Basically the field or parameter is basically all the props in the field is getting
+// passed into the renderField
+// With the props passed in, it makes it pretty 'universal' to all the Fields in the form
+const renderField = (field) => {
+  console.log(field)
+  return (
+  <input {...field.input} type = {field.type} placeholder = {field.placeholder} />
+  )
+}
+
+
+class ReduxFormTest extends React.Component{
 
     render(){
       console.log(this.props)
+      // handleSubmit will actually run this.prop.onSubmit
       const {handleSubmit} = this.props;
+      // For the component of the fields you can create your own stateles function
+      // to be put in there but it has to be outisde of your render
+      // You can call an <input/> into the field component
+      // You can put in initial values when you call the props initialValue
+      // you can actually call it and modify it in a higher order component and it will change
       return(
         <form onSubmit= {handleSubmit}>
           <div>
             <label htmlFor = 'title'> Title</label>
-            <Field name = 'title' component= 'input' type= 'text'/>
+            <Field
+            name = 'title'
+            component= {renderField}
+            type= 'text'
+            />
           </div>
           <div>
             <label htmlFor = 'content'> Content</label>
-            <Field name = 'content' component= 'input' type= 'text'/>
+            <Field name = 'content' component= {renderField} type= 'text'/>
           </div>
           <div>
-            <label htmlFor = 'strat_time'>Start Time</label>
-            <Field name = 'start_time' component= 'input' type= 'date'/>
+            <label htmlFor = 'start_time'>Start Time</label>
+            <Field name = 'start_time' component= {renderField} type= 'date'/>
           </div>
           <div>
-            <label htmlFor = 'end_time'>Email</label>
-            <Field name = 'end_time' component= 'input' type= 'date'/>
+            <label htmlFor = 'end_time'>End Time</label>
+            <Field name = 'end_time' component={renderField} type= 'date'/>
           </div>
           <div>
-            <label htmlFor = 'location'>Email</label>
-            <Field name = 'location' component= 'input' type= 'text'/>
+            <label htmlFor = 'location'>Location</label>
+            <Field name = 'location' component= {renderField} type= 'text'/>
           </div>
           <button type = 'submit'>Submit</button>
         </form>
@@ -46,8 +75,21 @@ class ReduxFormTest extends React.Component{
 // When you wrap your whole from around with redux form, redux form provides you
 // will all sort of functions that is prep filled in redux form
 // You can also design your own functions based on the given props to be put into the props too
+
+// The redux form is what communicates with the store, it will provide props about
+// the form state and functions to handle submission
+
+// In order to modify this you have to call a constant outside the class
 ReduxFormTest = reduxForm({
   form: 'contact', //you will give the form a name
+  enableReinitialize: true,
 })(ReduxFormTest);
 
+
+
+
+// The gist of redux form is bascially the redux form can be modified and such and then
+// you can pass those inputs into an action created by the redux form then those actions will
+// be dispatched in into the reducers then the reduces will change the states and then if there is
+// an onchange or whatever, changes the Fields in the forms
 export default ReduxFormTest;
