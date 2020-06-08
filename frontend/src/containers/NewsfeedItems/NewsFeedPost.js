1b827ebf1004b3ee9afa7367da27cd6169995603
pinghsu520 <pinghsu520@gmail.com>
2020-06-08 15:16:57 -0700

    image card added in

--- frontend/src/containers/NewsfeedItems/NewsFeedPost.js
+++ frontend/src/containers/NewsfeedItems/NewsFeedPost.js
@@ -46,6 +46,107 @@ class NewsfeedPost extends React.Component {
   }
 
 
+  ContentOfPic() {
+    let temp="http://127.0.0.1:8000"+this.props.data.image;
+    const success = () => {
+    message.success('Clipped to your album!');
+    };
+    return(
+      <Card
+         className="cardborder"
+         style={{ width: 700, marginTop: 16,  }}
+         actions={[
+           <SettingOutlined key="setting" />,
+           <EditOutlined key="edit" />,
+           <EllipsisOutlined key="ellipsis" />,
+         ]}
+       >
+
+
+          <span>
+          <Popover content={
+            <div>
+
+
+              <a href="/current-user" className="nav-text">
+                {this.props.data.user.username}
+              </a>
+              <p>Content</p>
+            </div>
+            }
+            title={this.props.data.user.username}
+            placement="topLeft"
+            >
+           <Button> {this.props.data.user.username} </Button>
+          </Popover>
+
+          </span>
+          <br></br>
+          <span>
+            <b>Date:</b> {this.props.data.created_at}
+          </span>
+          <br></br>
+          <h2 className="RobotoFont">
+            {this.props.data.caption}
+          </h2>
+
+          <ArrowRightOutlined
+          style={{ marginLeft: 550 }}
+          size="32px"
+          onClick={success}
+
+
+          />
+          <ShareAltOutlined
+          style={{ marginLeft: 550 }}
+
+          />
+          <br></br>
+
+
+
+          {
+              this.props.data.image ?
+
+              // <div style="max-width: 100%; max-height: 100%;">
+                <span >
+                  <img src= {temp} height="250" width="450"/>
+                </span>
+              // </div>
+
+              :
+              <div></div>
+
+
+          }
+
+          <div class="small ui labeled button" tabindex="0"
+
+              onClick ={this.AddOneToLike}
+          >
+            <div class="small ui red button">
+              <i class="heart icon"></i> Like
+            </div>
+            <a class="ui basic red left pointing label">
+              {this.props.data.like_count}
+
+
+
+
+            </a>
+
+
+          </div>
+
+
+
+
+
+        </Card>
+
+    )
+  }
+
   // this renders the posts on the newsfeed
 
   ContentOfPost(){
@@ -274,16 +375,27 @@ class NewsfeedPost extends React.Component {
         onCancel={this.handleCancel}
         width="800px"
       >
-      <p>{this.ContentOfPost()}</p>
-      <p>{this.ContentofComments()}</p>
+        <p>{this.ContentOfPost()}</p>
+        <p>{this.ContentofComments()}</p>
 
       </Modal>
 
 
 
+      {
+              this.props.data.image ?
+           <p>{this.ContentOfPic()}</p>
 
-      {this.ContentOfPost()}
+      //
+              :
+      //         <div></div>
+      //
+      //
+      //ContentOfPic
+      <p> {this.ContentOfPost()} </p>
 
+
+      }
 
 
 
