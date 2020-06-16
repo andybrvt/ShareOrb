4eedd2a36c19235657cd17c91b9b4b367c335fab
pinghsu520 <pinghsu520@gmail.com>
2020-06-15 20:44:45 -0700

    sidebar changes

--- frontend/src/components/SideMenu/SideMenu.js
+++ frontend/src/components/SideMenu/SideMenu.js
@@ -11,8 +11,8 @@ import {
 import {Link} from 'react-router-dom';
 import * as dateFns from 'date-fns';
 import testPic from './antd.png';
-import { Drawer, Layout, LocaleProvider, Icon,Row, Col, Dropdown,  Menu, Breadcrumb} from 'antd';
-
+import { Drawer, Layout, LocaleProvider, Icon,Row, Col, Dropdown,  Menu, Breadcrumb, Space} from 'antd';
+import "./SideMenu.css"
 
 const { Header, Sider, Content } = Layout;
 
@@ -30,23 +30,43 @@ class SideMenu extends React.Component {
   render() {
     return (
       <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<HeartTwoTone twoToneColor="#eb2f96" />}>
-            nav 1
-          </Menu.Item>
-            <Menu.Item key="2" icon={<UserOutlined />}>
-              <a href='/'>
 
-                  Person
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+
+              <img  class="logo" src={testPic}  style={{width:100, height:100}} />
+
+
+          <Menu theme="dark"
+           defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark">
+
+          
+          <Menu.Item key="1">
+            <a href='/home'>
+                <Icon type="home" />
+                Home
             </a>
+          </Menu.Item>
+
+
+            <Menu.Item key="2">
+            <Icon type="user" />
+                  Find Friends
             </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
+            <Menu.Item key="3">
+
               <a href='/chat/1'>
-                  <Icon name='large comments icon' />
+                  <Icon type="inbox" />
                   Messages
-            </a>
+              </a>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <a href='/personalcalendar/2020/6'>
+                  <Icon type="calendar" />
+                  Personal calendar
+              </a>
             </Menu.Item>
           </Menu>
         </Sider>
@@ -64,8 +84,8 @@ class SideMenu extends React.Component {
             className="site-layout-background"
             style={{
 
-              padding: 24,
-              minHeight: 280,
+              padding: 30,
+              minHeight: 800,
             }}
           >
           <HeartTwoTone twoToneColor="#eb2f96" />
