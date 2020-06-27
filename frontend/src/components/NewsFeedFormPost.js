2fad380ea8fbf20a2afc018238feafd678b7672d
pinghsu520 <pinghsu520@gmail.com>
2020-06-26 18:01:59 -0700

    working on fix for upload picture

--- frontend/src/components/NewsFeedFormPost.js
+++ frontend/src/components/NewsFeedFormPost.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 import { authAxios } from './util';
 import { connect } from "react-redux";
 import { Input } from 'antd';
-import {Button} from 'antd';
-
+import {Button, Upload, message} from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 const NewsFeedFormPost = (props) => {
   // formData = new FormData();
   const {token} = props;
@@ -20,6 +20,32 @@ const NewsFeedFormPost = (props) => {
 
   const { TextArea } = Input;
 
+
+const uploadImageRainbowProgress = {
+
+   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+
+   onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  progress: {
+      strokeColor: {
+        '0%': '#108ee9',
+        '100%': '#87d068',
+      },
+      strokeWidth: 3,
+      format: percent => `${parseFloat(percent.toFixed(2))}%`,
+    },
+
+
+}
 
   useEffect(() => {
     console.log('hit')
@@ -115,11 +141,30 @@ const NewsFeedFormPost = (props) => {
 	    <div className="upload-container">
 				{ stage !== "image" ?
 				<div  className= "uploadImage upload" >
-                <input type="file" name="image" onChange= {onChange} />
-				</div> :
+
+                {/* <input type="file" name="image" onChange= {onChange} /> */}
+                <Upload {...props} style={{marginBottom:100}}>
+                   <Button >
+                     <UploadOutlined {...uploadImageRainbowProgress} /> Click to Upload
+                   </Button>
+                 </Upload>
+
+
+
+
+
+				</div>
+
+
+
+
+        :
+
+
 				<div  className= "uploadImage upload " >
 
 				</div>}
+
         <TextArea rows={4} type="text" name="caption" onChange= {onChange}value={caption}
         style={{width: '600px'}}/>
 
@@ -127,8 +172,8 @@ const NewsFeedFormPost = (props) => {
 				<div className="submit">
                 <Button onClick={onFormSubmit}>Post</Button>
 				</div>
-				</div>
-            </form>
+			</div>
+    </form>
 	</Container>
   );
 };
