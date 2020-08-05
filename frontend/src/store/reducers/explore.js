f114f36b0d9cb4f44839ce7dcc9a6973ae0406cf
andybrvt <andybrvt@gmail.com>
2020-08-04 17:08:58 -0700

    got the unfollowing done

--- frontend/src/store/reducers/explore.js
+++ frontend/src/store/reducers/explore.js
@@ -64,24 +64,81 @@ export const closeChangeProfilePic = (state, action) => {
   })
 }
 
-// export const addUnFollowing = (state, action) => {
-//   let profiles  = state.profiles
-//   let person_index = ''
-//   let person_profile = []
-//   for (let i = 0; i <profiles.length; i++){
-//       if (profiles[i].username === action.followObject.actorUsername )
-//   }
-//
-//   return updateObject (state, {
-//
-//   })
-// }
-//
-// export const addUnFollower = (state, action) => {
-//   return updateObject (state, {
-//
-//   })
-// }
+export const addUnFollowing = (state, action) => {
+  let profiles  = state.profiles
+  let person_index = ''
+  let person_profile = []
+  let target_profile = []
+  // The target_profile will be that of the other person
+  // The perosn_profile will be that of the current user profile
+  for (let i = 0; i <profiles.length; i++){
+      if (profiles[i].username === action.followObject.user ){
+        // This will remove the target from the user's following
+        person_index = profiles[i]['get_following'].indexOf(action.followObject.person_unfollowing)
+        person_profile = profiles[i]['get_following']
+        person_profile.splice(person_index, 1)
+      }
+      if (profiles[i].username === action.followObject.person_unfollowing){
+        // This is to remove the actor from the targets following
+        person_index = profiles[i]['get_followers'].indexOf(action.followObject.user )
+        target_profile = profiles[i]['get_followers']
+        target_profile.splice(person_index, 1)
+      }
+  }
+
+  return updateObject (state, {
+    profiles: state.profiles.map(
+      profile => profile.username === action.followObject.user ? {
+        ...profile,
+        get_following: person_profile
+      } : profile.username === action.followObject.person_unfollowing ? {
+        ...profile,
+        get_followers: target_profile
+      } : profile
+    )
+  })
+}
+
+export const addUnFollower = (state, action) => {
+  // The process of this will be similar to the addUnFollowing but pretty much
+  // the users will be switch up because the mina user will be the other person
+  // so that we have to remove follower from them and then remove a following
+  // from the other person
+
+  let profiles = state.profiles
+  let person_index = ''
+  let person_profile = []
+  let target_profile = []
+  // In this case the person_profile will be that of the person that got the following
+  // we will be removing a follower from that person
+  // The target_profile will be the follower and then this function will will then remove
+  // the following from that person
+  for (let i = 0; i < profiles.length; i++ ){
+    if(profiles[i].username === action.followObject.user){
+      person_index = profiles[i]['get_followers'].indexOf(action.followObject.person_unfollower)
+      person_profile = profiles [i]['get_followers']
+      person_profile.splice(person_index, 1)
+    }
+    if(profiles[i].username === action.followObject.person_unfollower){
+      person_index = profiles[i]['get_following'].indexOf(action.followObject.user)
+      target_profile = profiles[i]['get_following']
+      target_profile.splice(person_index, 1)
+    }
+
+  }
+
+  return updateObject (state, {
+    profiles: state.profiles.map(
+      profile => profile.username === action.followObject.user ? {
+        ...profile,
+        get_followers: person_profile
+      } : profile.username === action.followObject.person_unfollower ? {
+        ...profile,
+        get_following: target_profile
+      } : profile
+    )
+  })
+}
 
 export const addFollowing = (state, action) => {
   // probally gonna have to think of a way to do the binary search here
@@ -119,10 +176,10 @@ const reducer = (state = initialState, action) => {
       return openChangeProfilePic(state, action)
     case actionTypes.CLOSE_CHANGE_PROFILE_PIC:
       return closeChangeProfilePic(state, action)
-    // case actionTypes.ADD_UNFOLLOWING:
-    //   return addUnFollowing(state, action)
-    // case actionTypes.ADD_UNFOLLOWER:
-    //   return addUnFollower(state, action)
+    case actionTypes.ADD_UNFOLLOWING:
+      return addUnFollowing(state, action)
+    case actionTypes.ADD_UNFOLLOWER:
+      return addUnFollower(state, action)
     default:
       return state;
   };
