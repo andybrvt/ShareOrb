64627544842c0cf81313910db9cea3119cf32d27
pinghsu520 <pinghsu520@gmail.com>
2020-08-20 18:16:47 -0700

    minor shifting of avatar group

--- frontend/src/containers/NewsfeedItems/UserAvatar.js
+++ frontend/src/containers/NewsfeedItems/UserAvatar.js
@@ -14,8 +14,8 @@ class UserAvatar extends React.Component{
   render(){
     var num=Math.floor(Math.random() * 4);
 
-    const avatarColor=['#fde3cf', '#b5f5ec', '#d3f261', '#b37feb', '#1890ff']
-    const textColor=['#f56a00', '#13c2c2', '#7cb305', '	#FFFFFF', '#FFFFFF']
+    const avatarColor=[]
+    const textColor=[]
     return (
       <span>
 
