82bcc028f4bea00a85ad7827b014f302487b57f5
pinghsu520 <pinghsu520@gmail.com>
2020-09-04 20:01:53 -0700

    adding comments in for now, functionality still needs to start

--- frontend/src/containers/comments/PreviewComments.js
+++ frontend/src/containers/comments/PreviewComments.js
@@ -1,103 +1,85 @@
 import { Comment, Tooltip, List, Divider } from 'antd';
 import moment from 'moment';
-import React from "react";
+import React, { createElement } from 'react';
 import './PreviewComments.css'
-
+import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons';
 class Comments extends React.Component {
 
   state = {
+    likes:0,
+    dislike:0,
+    action:'',
+  };
 
+  like = () => {
+    this.setState({
+      likes:this.state.likes+1,
+      action:'liked'
+    });
+  };
+
+
+  dislike = () => {
+    this.setState({
+      dislike:this.state.dislike+1,
+      action:'disliked'
+    });
+  };
+
+
+
+   render(){
+
+     const actions=[
+
+         <Tooltip key="comment-basic-like" title="Like">
+           <span>
+
+              <LikeFilled/>
+
+             <span className="comment-action">{2}</span>
+           </span>
+         </Tooltip>,
+
+         <span key="comment-basic-reply-to">Reply to</span>
+
+
+
+     ];
+
+     const data = [
+  {
+
+    datetime: (
+      <Tooltip
+        title={moment().subtract(2, "days").format("YYYY-MM-DD HH:mm:ss")}
+      >
+        <span>{moment().subtract(2, "days").fromNow()}</span>
+      </Tooltip>
+    )
   }
+];
 
-
-
-  render() {
-    console.log(this.props)
-    console.log(this.props.newsfeed)
-    console.log(this.props.newsfeed.data)
-    console.log(this.props.newsfeed.data.post_comments)
-
-    const data = [
-      {
-        actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: (
-          <p>
-            We supply a series of design principles, practical patterns and high quality design
-            resources (Sketch and Axure), to help people create their product prototypes beautifully and
-            efficiently.
-          </p>
-        ),
-        datetime: (
-          <Tooltip
-            title={moment()
-              .subtract(1, 'days')
-              .format('YYYY-MM-DD HH:mm:ss')}
-          >
-            <span>
-              {moment()
-                .subtract(1, 'days')
-                .fromNow()}
-            </span>
-          </Tooltip>
-        ),
-      },
-      {
-        actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: (
-          <p>
-            We supply a series of design principles, practical patterns and high quality design
-            resources (Sketch and Axure), to help people create their product prototypes beautifully and
-            efficiently.
-          </p>
-        ),
-        datetime: (
-          <Tooltip
-            title={moment()
-              .subtract(2, 'days')
-              .format('YYYY-MM-DD HH:mm:ss')}
-          >
-            <span>
-              {moment()
-                .subtract(2, 'days')
-                .fromNow()}
-            </span>
-          </Tooltip>
-        ),
-
-
-      },
-    ];
-
-    let locale = {
-      emptyText: 'Abc',
-    };
     return (
-      <div class="fontTest maxHeightPost">
-        <List
-          // className="comment-list"
-
-          itemLayout="horizontal"
-          dataSource={this.props.newsfeed.data.post_comments.slice(0, 2)}
-          renderItem={item => (
-            <li class="commentLook">
-              <Comment
-
-                locale={locale}
-                actions={item.actions}
-                author={item.name}
-                avatar={"https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"}
-                content={item.body}
-                datetime={item.datetime}
-              />
-
-            </li>
-
-          )}
-        />
+      <div>
+      <List
+  className="comment-list"
+  header={`${data.length} replies`}
+  itemLayout="horizontal"
+  dataSource={this.props.newsfeed.data.post_comments.slice(0, 2)}
+  renderItem={(item) => (
+    <li>
+      <Comment
+      class="commentLook"
+      actions={actions}
+      author={item.name}
+      avatar={"https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"}
+      content={item.body}
+      datetime={"few seconds ago"}
+      />
+    </li>
+  )}
+/>
       </div>
     );
   }
