899dbb6476bfdfdac46ca84607addf6f817c4e62
pinghsu520 <pinghsu520@gmail.com>
2020-09-07 15:26:44 -0700

    fixed >3 users on avatar group

--- frontend/src/containers/NewsfeedItems/UserAvatar.js
+++ frontend/src/containers/NewsfeedItems/UserAvatar.js
@@ -13,7 +13,8 @@ class UserAvatar extends React.Component{
 
   render(){
     console.log(this.props)
-    let like_people = this.props.data.people_like
+    let like_people = this.props.like_people
+    console.log(like_people)
     var num=0;
     const avatarColor=['#bfbfbf']
     const textColor=['#FFFFFF']
@@ -26,26 +27,12 @@ class UserAvatar extends React.Component{
         maxStyle={{ color: textColor[num], backgroundColor: avatarColor[num] }}
         maxCount={3}
         >
-
-
-          <Tooltip placement="topLeft" title={`${like_people[0].first_name} ${like_people[0].last_name} `}>
-                <Avatar src={'http://127.0.0.1:8000'+like_people[0].profile_picture}/>
-          </Tooltip>
-
-          <Tooltip placement="topLeft" title={`${like_people[1].first_name} ${like_people[1].last_name} `}>
-                <Avatar src={'http://127.0.0.1:8000'+like_people[1].profile_picture}/>
-          </Tooltip>
-
-
-          <Tooltip placement="topLeft" title={`${like_people[2].first_name} ${like_people[2].last_name} `}>
-          <Avatar
-          src={'http://127.0.0.1:8000'+like_people[2].profile_picture}/>
-         </Tooltip>
-
-         <Tooltip placement="topLeft" title={`${like_people[3].first_name} ${like_people[3].last_name} `}>
-         <Avatar
-         src={'http://127.0.0.1:8000'+like_people[3].profile_picture}/>
+        
+        {this.props.like_people.map((user) => (
+          <Tooltip placement="topLeft" title={`${user.first_name} ${user.last_name} `}>
+        <Avatar src={'http://127.0.0.1:8000'+user.profile_picture}/>
         </Tooltip>
+      ))}
 
 
         </Avatar.Group>
