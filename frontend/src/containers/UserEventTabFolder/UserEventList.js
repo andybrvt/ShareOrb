17e0973ccbf23fd97ea6560ae1e8134dab2b906f
andybrvt <andybrvt@gmail.com>
2020-11-22 16:06:08 -0700

    added in basic information of event cell with some css

--- frontend/src/containers/UserEventTabFolder/UserEventList.js
+++ frontend/src/containers/UserEventTabFolder/UserEventList.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import './UserEventList.css';
 import {PictureOutlined } from '@ant-design/icons';
+import * as dateFns from 'date-fns';
 
 
 
 class UserEventList extends React.Component{
+
+  capitalize (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  dateView(date) {
+    // This will be presenting the calendar day on the modal
+    // console.log(dateFns.format(new Date(date), ''))
+    console.log(date)
+    let month = ''
+    // let day = ''
+    if (date !== ''){
+      month = dateFns.format(new Date(date), 'MMM d, yyyy')
+    }
+
+    // console.log(month)
+    return (
+
+      month
+
+
+    )
+
+  }
+
+  timeFormater(time){
+    // This will change the format of the time properly to the 1-12 hour
+    console.log(time)
+    const timeList = time.split(':')
+    let hour = parseInt(timeList[0])
+    let minutes = timeList[1]
+    var suffix  = hour >= 12 ? "PM":"AM"
+
+    console.log(11%12)
+    hour = ((hour+11)%12+1)+':'+minutes+" "+ suffix
+    return hour
+
+  }
+
+
 
   renderEventCells = () => {
     console.log(this.props)
@@ -33,6 +74,17 @@ class UserEventList extends React.Component{
               src = {'http://127.0.0.1:8000'+socialEventList[i].backgroundImg} />
             </div>
           }
+
+          <div className = "eventBoxTitle">{this.capitalize(socialEventList[i].title)}</div>
+          <div className = "eventBoxContent">Content: {socialEventList[i].content} </div>
+          <div className = "eventBoxLocation">
+          <i class="fas fa-map-marker-alt"></i> {socialEventList[i].location}</div>
+          <div className = "eventBoxDay">
+          <i class="far fa-calendar"></i> {this.dateView(socialEventList[i].event_day)}
+          </div>
+          <div className = "eventBoxTimes"><i class="far fa-clock"></i> {this.timeFormater(socialEventList[i].start_time)} - {this.timeFormater(socialEventList[i].end_time)}</div>
+
+          <div className = "eventBoxHost "> Host: {socialEventList[i].host.username}</div>
 
           </div>
         )
