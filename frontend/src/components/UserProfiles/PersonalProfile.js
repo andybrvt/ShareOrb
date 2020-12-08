742f1d2be05218930b37442bcf75e5a318c68b29
pinghsu520 <pinghsu520@gmail.com>
2020-12-07 20:12:18 -0800

    small fix

--- frontend/src/components/UserProfiles/PersonalProfile.js
+++ frontend/src/components/UserProfiles/PersonalProfile.js
@@ -698,7 +698,7 @@ class PersonalProfile extends React.Component{
 
       return(
         <div className = {`profilePage ${this.props.location.state ? "active" : ""}`}>
-          <div class="eventCard" style={{left:'15%',width:'1200px', height:'375px', }}>
+          <div class="eventCard" style={{left:'22%',width:'1000px', height:'375px', }}>
             <div class="parentFlexContainer">
               {this.renderProfilePic()}
               <span className = 'profileName'>
