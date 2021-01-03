b64eef33cf959412e4de9a0d21c8492c3837b3ef
pinghsu520 <pinghsu520@gmail.com>
2021-01-02 17:11:44 -0800

    deploy test on s3

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '41+q0&=-%76d_@&zf&=g2c8tbp31-fts867t&q#dj^o^o_e(lf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eb-virt.eba-paq3ygtm.us-west-2.elasticbeanstalk.com']
-
+ALLOWED_HOSTS = ['127.0.0.1:8000','eb-virt.eba-paq3ygtm.us-west-2.elasticbeanstalk.com']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
