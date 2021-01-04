de46cf8104de028a4ca6886681c24169f06b0a69
andybrvt <andybrvt@gmail.com>
2021-01-03 23:04:22 -0700

    deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -109,7 +109,7 @@ if 'aaotw5bj0aq6b7.cyxbd89sft5i.us-west-1.rds.amazonaws.com' in os.environ:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['ebdb'],
             'USER': os.environ['shareorbDB'],
-            'PASSWORD': os.environ['pingandy123'],
+            'PASSWORD': os.environ['Pingandy123'],
             'HOST': os.environ['aaotw5bj0aq6b7.cyxbd89sft5i.us-west-1.rds.amazonaws.com'],
             'PORT': os.environ['3306'],
         }
