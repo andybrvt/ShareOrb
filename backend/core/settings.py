b8d42a31d1869d267231682fef74fc4fa8ad3e07
andybrvt <andybrvt@gmail.com>
2021-01-10 14:55:39 -0700

    deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '41+q0&=-%76d_@&zf&=g2c8tbp31-fts867t&q#dj^o^o_e(lf'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['api.shareorb.com','shareorb-env.eba-zm7j48gb.us-west-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['172.31.4.121', 'api.shareorb.com','shareorb-env.eba-zm7j48gb.us-west-1.elasticbeanstalk.com']
 
 # ALLOWED_HOSTS = []
 
