cb29f83e8611034b09ebca70b94fc292395daae2
andybrvt <andybrvt@gmail.com>
2021-01-10 17:32:31 -0700

    deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -225,7 +225,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": ["redis://shareorbredisnew.bz41az.ng.0001.usw1.cache.amazonaws.com:6379"],
+            "hosts": ["redis://(shareorbredisnew.bz41az.ng.0001.usw1.cache.amazonaws.com,6379)"],
         },
     },
 }
