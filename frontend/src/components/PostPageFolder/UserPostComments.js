5693bd8724be01c63fd51b5023b6763fa6f63304
pinghsu520 <pinghsu520@gmail.com>
2021-01-15 19:41:24 -0800

    modal changes

--- frontend/src/components/PostPageFolder/UserPostComments.js
+++ frontend/src/components/PostPageFolder/UserPostComments.js
@@ -93,7 +93,7 @@ class UserPostComments extends React.Component{
               <div className = 'postCommentItem'>
 
                 <div className = 'postCommentNameTag'>
-                  <Avatar size = {35} src = {`${global.IMAGE_ENDPOINT}`+item.commentUser.profile_picture} />
+                  <Avatar size = {30} src = {`${global.IMAGE_ENDPOINT}`+item.commentUser.profile_picture} />
                   <div className = 'postCommentName'>
                     <div className = 'postCommentUsername'>
                       <b>{this.capitalize(item.commentUser.first_name)} {this.capitalize(item.commentUser.last_name)} </b>
