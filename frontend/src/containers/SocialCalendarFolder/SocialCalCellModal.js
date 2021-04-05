ce4f0bf3580cbaf497eb78469bde7e55e55990c0
andybrvt <andybrvt@gmail.com>
2021-04-04 23:53:40 -0700

    added esc key for the modal

--- frontend/src/containers/SocialCalendarFolder/SocialCalCellModal.js
+++ frontend/src/containers/SocialCalendarFolder/SocialCalCellModal.js
@@ -6,11 +6,34 @@ import SocialCalCellPage from "./SocialCalCellPage";
 
 class SocialCalCellModal extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.escFunction = this.escFunction.bind(this);
+  }
+
+  escFunction(event){
+    // 27 is the keycode for esc
+    if(event.keyCode === 27){
+      this.back(event)
+    }
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.escFunction, false);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.escFunction, false);
+
+  }
+
   back = e => {
     e.stopPropagation();
     this.props.history.goBack();
 
   }
+
+
 
   render(){
 
